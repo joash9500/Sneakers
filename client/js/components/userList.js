d984aacdc197b6a8262017320678a1b169896984
joash teng <joashteng123@gmail.com>
2022-06-17 21:14:30 +0800

    users page

--- client/js/components/userList.js
+++ client/js/components/userList.js
@@ -1,3 +1,27 @@
 function renderUsers() {
-    
+    const htmlContent = document.getElementById('content')
+
+    axios.get('/api/users').then((resp) => {
+        htmlContent.innerHTML = ''
+        const title = document.createElement('h1')
+        title.innerText = 'Our Members'
+        htmlContent.appendChild(title)
+
+        userData = resp.data
+        userData.forEach(elm => {
+            const userName = document.createElement('h3')
+            userName.innerText = elm.name
+            const userLocation = document.createElement('p')
+            userLocation.innerText = 'Location: ' + elm.location
+            const userSize = document.createElement('p')
+            userSize.innerText = 'Ideal shoe size: ' + elm.ideal_size
+            const userPhoto = document.createElement('img')
+            userPhoto.src = elm.photo_path
+            
+            // htmlContent.appendChild(userName)
+            htmlContent.append(userName, userPhoto, userLocation, userSize)
+            // htmlContent.append(userName, userLocation, userSize, userPhoto)
+        })
+
+    })
 }
